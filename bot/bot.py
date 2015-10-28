1adff9753d1a575a11bee925719f25316cb4bd41
konstantinmalysh <konstantinmalysh@gmal.com>
2015-10-28 19:48:09 +0300

    Bot: added some commands
    Added two commands; '/more' for getting another answer, '/exit' for
    ending last operation. Added simple keyboard for these two commands.

--- bot/bot.py
+++ bot/bot.py
@@ -1,29 +1,55 @@
 import telegram
+import json
 
 TOKEN = StackOverflowYaBot.token
 
 LAST_UPDATE_ID = None
+LAST_MESSAGE = None
+IS_KEYBOARD_WORKING = False
+commands = [['/start'], ['/more'], ['/exit']]
 
-commands = ['/start']
 
+def execute(cmd, bot, chat_id):
+    global LAST_MESSAGE, IS_KEYBOARD_WORKING
 
-##TODO: execute commands
+    if cmd.startswith('/start'):
+        bot.sendMessage(chat_id=chat_id,
+                        text='Welcome to Stack Overflow search bot!')
+
+    elif cmd.startswith('/more'):
+        bot.sendMessage(chat_id=chat_id,
+                        text=LAST_MESSAGE)
+
+    elif cmd.startswith('/exit'):
+        if IS_KEYBOARD_WORKING:
+            bot.sendMessage(chat_id=chat_id, text="Exited successfully!",
+                            reply_markup=telegram.ReplyKeyboardHide())
+            IS_KEYBOARD_WORKING = False
+        else:
+            bot.sendMessage(chat_id=chat_id, text="Nothing to exit")
+
+    else:
+        bot.sendMessage(chat_id=chat_id, text="Unknown command")
+
 
 def answer(bot):
-    global LAST_UPDATE_ID
+    global LAST_UPDATE_ID, LAST_MESSAGE, IS_KEYBOARD_WORKING
     for update in bot.getUpdates(offset=LAST_UPDATE_ID):
         chat_id = update.message.chat_id
         message = update.message.text.encode('utf-8')
         dec_message = message.decode('utf-8')
 
         if message:
+            reply_markup = {'keyboard': commands[1::],
+                            'resize_keyboard': True, 'one_time_keyboard': False}
+            reply_markup = json.dumps(reply_markup)
             if not dec_message.startswith('/'):
+                IS_KEYBOARD_WORKING = True
                 bot.sendMessage(chat_id=chat_id,
-                                text=dec_message)
+                                text=dec_message, reply_markup=reply_markup)
+                LAST_MESSAGE = dec_message
             else:
-                if dec_message not in commands:
-                    bot.sendMessage(chat_id=chat_id,
-                                    text='Unknown command')
+                execute(dec_message, bot, chat_id)
 
             LAST_UPDATE_ID = update.update_id + 1
 
