be68290b78d5ad22ac8bc84ca0a063b7ffe43054
mikhail <pershin.m.r@gmail.com>
2015-11-08 14:43:02 +0300

    updated sql_to_elastic.py script: no more multiple requests

--- utils/sql_to_elastic.py
+++ utils/sql_to_elastic.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import json
+import copy
 
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
@@ -16,59 +17,83 @@ db_config = {
 document_template = {
 	'_index' : 'stackoverflowdump',
 	'_type' : 'posts',
-	'_id' : None,
+	#'_id' : None,
 	'doc' : {
-		'title' : None,
-		'body' : None,
-		'answer' : None
+		'question' : {
+			'Id' : None,
+			'Title' : None,
+			'Body' : None,
+			'Tags' : None,
+			'AnswerCount' : None,
+			'FavoriteCount' : None,
+			'Score' : None,
+			'ViewCount' : None,
+		},
+		'answer' : {
+			'Id' : None,
+			'Body' : None,
+			'Score' : None,
+		},
+
 	}
 }
 
-select_query = 'SELECT * FROM {table} WHERE Id = {id}';
+select_query = """SELECT
+					questions.Id AS question_id,
+					questions.Title AS question_title,
+					questions.Body AS question_body,
+					questions.Tags AS question_tags,
+					questions.AnswerCount AS question_answercount,
+					questions.FavoriteCount AS question_favoritecount,
+					questions.Score AS question_score,
+					questions.ViewCount AS question_viewcount,
+					answers.Id AS answer_id,
+					answers.Body AS answer_body,
+					answers.Score AS answer_score
+					FROM posts AS answers JOIN posts AS questions ON (answers.ParentId = questions.Id) WHERE answers.PostTypeId = 2;""";
 
 
 # Script body
 es = Elasticsearch()
 
-cnx1 = mysql.connector.connect(**db_config)
-cnx2 = mysql.connector.connect(**db_config)
+cnx = mysql.connector.connect(**db_config)
 
-cursor = cnx1.cursor(dictionary=True)
-cursor.execute('SELECT * FROM posts;')
+cursor = cnx.cursor(dictionary=True)
+cursor.execute(select_query)
 
+indexed_posts_cnt = 0
 posts_to_index_batch = []
 for post in cursor:
+
+	document_template['doc']['question']['Id'] = post['question_id']
+	document_template['doc']['question']['Title'] = post['question_title']
+	document_template['doc']['question']['Body'] = post['question_body']
+	document_template['doc']['question']['Tags'] = post['question_tags']
+	document_template['doc']['question']['AnswerCount'] = post['question_answercount']
+	document_template['doc']['question']['FavoriteCount'] = post['question_favoritecount']
+	document_template['doc']['question']['Score'] = post['question_score']
+	document_template['doc']['question']['ViewCount'] = post['question_viewcount']
+	document_template['doc']['answer']['Id'] = post['answer_id']
+	document_template['doc']['answer']['Body'] = post['answer_body']
+	document_template['doc']['answer']['Score'] = post['answer_score']
 	
-	
-	if (post['PostTypeId'] == 1) and (post['AcceptedAnswerId'] is not None):
-		inner_cursor = cnx2.cursor(dictionary=True, buffered=True)
-
-		inner_cursor.execute(select_query.format(
-			table='posts',
-			id=post['AcceptedAnswerId']
-			))
-
-		answer = inner_cursor.fetchone()
-		if answer is None:
-			print('Post id = {id} is missing!'.format(id=post['AcceptedAnswerId']))
-			continue
-
-		document_template['_id'] = post['Id']
-		document_template['doc']['title']  = post['Title']
-		document_template['doc']['body']   = post['Body']
-		document_template['doc']['answer'] = answer['Body']
-		posts_to_index_batch.append(document_template.copy())
+	posts_to_index_batch.append(copy.deepcopy(document_template))
 
 	if len(posts_to_index_batch) == 10000:
-		try:
+		try:	
 			helpers.bulk(es, posts_to_index_batch)
+
 			posts_to_index_batch = []
-			print('Batch indexed')
+
+			indexed_posts_cnt += 1;
+			print('Batch {n} indexed'.format(n=indexed_posts_cnt))
+
 		except:
-			print('Exception rised at batch Indexing')
+			print('Exception rised at batch indexing')
 
 
 if len(posts_to_index_batch) > 0:
 	helpers.bulk(es, posts_to_index_batch)
+
 
 print('Indexing complete!')
