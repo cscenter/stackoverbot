6e2939d918947b83aff72bb4ef82e6396d81fe6c
mikhail <pershin.m.r@gmail.com>
2015-12-08 02:22:59 +0300

    Search handler added

---
+++ utils/search_handler.py
@@ -0,0 +1,38 @@
+import http.server
+
+from elasticsearch import Elasticsearch
+from urllib.parse import urlparse, parse_qs
+
+
+HANDLER_HOST_NAME = 'localhost'
+HANDLER_PORT_NUMBER = 9000
+ELASTICSEARCH_SETS = {'host': 'localhost', 'port': 9200}
+ELASTICSEARCH_INDEX = 'stackoverflowdump2'
+
+
+def search(query):
+    es = Elasticsearch(**ELASTICSEARCH_SETS)
+    search_hits = es.search(index=ELASTICSEARCH_INDEX, body={"query": {"match": {"Body": query}}})
+    res = [hit['_source']['doc'] for hit in search_hits['hits']['hits']]
+    return res
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        params = parse_qs(urlparse(self.path).query)
+        search_query = params['q'][0]
+        search_results = search(search_query)
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(str(search_results[0]['answer']['Id']), 'utf-8'))
+
+if __name__ == '__main__':
+    server_class = http.server.HTTPServer
+    httpd = server_class((HANDLER_HOST_NAME, HANDLER_PORT_NUMBER), MyHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
